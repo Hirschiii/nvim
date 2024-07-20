6e096a647812f9873cb8db89bfff9a002dede822
Niklas von Hirschfeld <niklas@vonhirschfeld.eu>
2024-07-20 12:06:32 +0200

    nvim harpoon, next and prev

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -8,6 +8,7 @@ return {
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end)
+
 		vim.keymap.set("n", "<C-e>", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
@@ -18,5 +19,8 @@ return {
 				harpoon:list():select(idx)
 			end)
 		end
+
+		vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+		vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 	end,
 }
